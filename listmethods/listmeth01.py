409a0f4d8ac6fde7316a649ba4a7ef7aa5bc8848
Breana Peters <BreanaP@Tlgcohort.com>
2024-08-06 16:18:13 +0000

    learning about dictionaries

---
+++ listmethods/listmeth01.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+proto = ["ssh", "http", "https"]
+print(proto)#!/usr/bin/env python3
+proto = ["ssh", "http", "https"]
+print(proto)
+
---
+++ listmethods/listmeth02.py
@@ -0,0 +1,1 @@
+python3 ~/mycode/listmethods/listmeth02.py
